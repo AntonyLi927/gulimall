2e432e7863064d9cd54f501a4d572aeec12d517c
lihancheng <lihancheng927@gmail.com>
2022-08-25 15:38:38 +0800

    initialization

---
+++ gulimall-member/src/test/java/com/antony/gulimall/gulimallmember/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antony.gulimall.gulimallmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
